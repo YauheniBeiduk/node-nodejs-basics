5f6104dfc7c218f215642081e81afb2e795df30f
YauheniBeiduk <Yauheni_Beiduk@epam.com>
2022-12-04 20:24:58 +0400

    feat: add logic to worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'node:worker_threads';
+import path from 'node:path';
+import url from 'node:url';
+import os from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
-};
+    const countOfCpus = os.cpus().length;
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const pathToFile = path.join(__dirname, 'worker.js');
+    const promises = [];
+
+    for (let i = 0; i < countOfCpus; i++) {
+        const results = new Promise((resolve) => {
+            const worker = new Worker(pathToFile, { workerData: 10 + i });
+            worker.on('message', (result) => {
+                resolve({ status: 'resolve', data: result });
+            });
+            worker.on('error', () => {
+                resolve({ status: 'error', data: null });
+            });
+        })
+        promises.push(results);
+    }
+
+    const calculations = await Promise.all(promises);
+
+    console.log(calculations);
+}
 
 await performCalculations();
