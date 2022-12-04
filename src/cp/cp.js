74ae30da936985c481e64e2dd0cfd54d4cd95017
YauheniBeiduk <Yauheni_Beiduk@epam.com>
2022-12-04 16:56:31 +0400

    feat: add logic to cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,20 @@
+import { spawn } from 'node:child_process';
+import url from 'node:url';
+import path from 'node:path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const __filename = path.join(__dirname, 'files', 'script.js');
+    const spawnChild = spawn('node', [__filename, ...args]);
+
+    process.stdin.on('data', (data) => {
+        spawnChild.stdin.write(data);
+    });
+
+
+    spawnChild.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
 };
 
-spawnChildProcess();
+spawnChildProcess([1,2,3,4,5,6,7,8,9]);
